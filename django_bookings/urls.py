016f9ed078b6d99b198e8ee38975894f26e88b8d
M. Samsioe <michael.samsioe@gmail.com>
2022-10-19 13:14:47 +0000

    Add templates for base and index test

--- django_bookings/urls.py
+++ django_bookings/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bookings.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', say_hello, name = 'say_hello'),
+    
 ]
