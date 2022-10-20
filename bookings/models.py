d60bce5b1a45840ea7f888e4c08c8f7c657b3f6c
M. Samsioe <michael.samsioe@gmail.com>
2022-10-20 14:48:32 +0000

    Fix bug for time in model Reservation

--- bookings/models.py
+++ bookings/models.py
@@ -3,16 +3,19 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+import django.utils.timezone
+
+
 STATUS =  ((0, 'Draft'), (1, 'Published'))
-STARS = ((1),(2),(3),(4),(5))
+
 
 # Create your models here.
 class Reservation(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     email = models.EmailField()
-    date = models.DateField(auto_now = True)
-    start_time = models.TimeField()
+    date = models.DateTimeField(default = django.utils.timezone.now)
+    #start_time = models.TimeField(auto_now= True)
     number_of_guests = models.IntegerField(default = 2, validators=[MaxValueValidator(12),
             MinValueValidator(1)])
     #featured_image = CloudinaryField('image', default = 'placeholder')
